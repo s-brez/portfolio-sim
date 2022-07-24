51c0fc6069f18c0076b7411cbfb0f9156a832a34
s-brez <sam@sdbgroup.io>
2022-07-24 16:41:19 +1000

    add summary method and discrete timeframe to test portfolio

--- Session 3 - Backtesting/portfolios.py
+++ Session 3 - Backtesting/portfolios.py
@@ -1,5 +1,6 @@
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
+import json
 
 from strategies import EMACross50200
 
@@ -19,12 +20,15 @@ class TestPortfolio():
 
         self.strategies = [EMACross50200]
 
+        # This implementation is limited to one timeframe.
+        self.timeframes = ["1d"]
+
         self.assets = {
             "EQUITIES": ["GOOGL", "AMZN", "TSLA", "F"],
             "CURRENCIES": ["EURUSD=X", "GBPUSD=X", "AUDUSD=X"],
             "COMMODITIES": ["GC=F", "ZO=F", "ZS=F", "KC=F"],
             "INDICES": ["^VIX", "^AXJO", "^GSPC", "^KS11", "DX-Y.NYB"],
-            "CRPYTO": ["BTC-USD"]
+            "CRYPTO": ["BTC-USD"]
         }
 
         self.assets_flattened = [i for j in self.assets.values() for i in j]
@@ -39,19 +43,19 @@ class TestPortfolio():
         # Percentage of asset class allocation to use for each strategy.
         self.strategy_allocations = {
             "EQUITIES": {
-                EMACross50200: 100,
+                EMACross50200.name: 100,
             },
             "CURRENCIES": {
-                EMACross50200: 100,
+                EMACross50200.name: 100,
             },
             "COMMODITIES": {
-                EMACross50200: 100,
+                EMACross50200.name: 100,
             },
             "INDICES": {
-                EMACross50200: 100,
+                EMACross50200.name: 100,
             },
-            "CRPYTO": {
-                EMACross50200: 100,
+            "CRYPTO": {
+                EMACross50200.name: 100,
             },
         }
 
@@ -60,3 +64,20 @@ class TestPortfolio():
 
         if self.acceptable_correlation_threshold < -1 or self.acceptable_correlation_threshold > 1:
             raise ValueError("Acceptable correlation value must be between -1 and 1.")
+
+    def summary(self) -> str:
+        return (
+            f"\n** {self.name} **"
+            f"\nOpening balance: {self.start_balance} {self.currency}"
+            f"\nStart date: {self.start_date}]"
+            f"\nMax open positions allowed: {self.max_simultaneous_positions}"
+            f"\nMax allowable correlation between positions: {self.acceptable_correlation_threshold}"
+            f"\nTarget instruments: {json.dumps(self.assets, indent=2)}"
+            f"\n\nAllocations per asset class (% of portfolio balance):"
+            f"\nEquities: {self.equities_allocation}"
+            f"\nCurrencies: {self.currencies_allocation}"
+            f"\nCommodities: {self.commodities_allocation}"
+            f"\nIndicies: {self.indices_allocation}"
+            f"\nCrypto: {self.crypto_allocation}"
+            f"\n\nStrategy allocations per asset class: (% of asset class allocation) {json.dumps(self.strategy_allocations, indent=2)}"
+        )
