cff22edd88073847c52bd4e51a646b365822e9ed
s-brez <sam@sdbgroup.io>
2022-07-24 16:40:34 +1000

    add per-timeframe correlation matrices

--- Session 3 - Backtesting/backtest.py
+++ Session 3 - Backtesting/backtest.py
@@ -1,6 +1,6 @@
 from os import listdir
 import pandas as pd
-
+import sys
 
 # pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -26,6 +26,7 @@ class Backtester:
     def __init__(self, portfolio):
         self.data = self.load_local_data(SYMBOLS)
         self.portfolio = portfolio
+        self.c_matrix = None
 
     def load_local_data(self, symbols: list) -> dict:
         """
@@ -93,6 +94,8 @@ class Backtester:
         Args:
             root: nested dict of dataframes as formatted by load_local_data().
             i.e data[asset_class][symbol][timeframe]
+            strategies: list of strategy objects from target portfolio.
+            symbols: flattened list of symbols from target portfolio.
 
         Returns:
             None (modifies root dictionary contents in place).
@@ -100,6 +103,8 @@ class Backtester:
         Raises:
             None.
         """
+
+        print("Applying feature data...")
 
         # Iterate all stored data.
         for asset_class in root.keys():
@@ -123,9 +128,107 @@ class Backtester:
                             index = pd.date_range(start, finish)
                             root[asset_class][symbol][timeframe] = root[asset_class][symbol][timeframe].reindex(index, fill_value=0)
 
-                            # print(symbol, timeframe)
-                            # print(root[asset_class][symbol][timeframe])
+                            # Add ticker column value, used later for correlation.
+                            root[asset_class][symbol][timeframe]['Ticker'] = symbol
 
-    def start(self):
+                        # print(symbol, timeframe)
+                        # print(root[asset_class][symbol][timeframe])
+
+        print("Feature data complete.")
+
+    def correlation_matrix(self, root: dict, timeframes: list, symbols: list) -> None:
+        """
+        Create correlation matrix from source datasets.
+
+        Args:
+            root: nested dict of dataframes as formatted by load_local_data().
+                e.g data[asset_class][symbol][timeframe]
+            timeframes: list of timeframes used by target portfolio.
+            timeframes: list of symbols used by target portfolio.
+
+        Returns:
+            c_matrix: dict of correlation matrices for each timeframe
+                e.g c_matrix[1d]
+
+        Raises:
+            None.
+        """
+
+        c_matrix = {}
+
+        # Aggregate close prices for each timeframe.
+        for timeframe in timeframes:
+
+            data_to_agg = []
+
+            for asset_class in root.keys():
+                for symbol in root[asset_class].keys():
+
+                    # Only run correlation for datasets we need.
+                    if symbol in symbols:
+
+                        data_to_agg.append(root[asset_class][symbol][timeframe])
+
+            agg_data = pd.concat(data_to_agg).reset_index()
+            agg_data.columns.values[0] = "Date"
+            agg_data = agg_data[['Date', 'Ticker', 'Close']]
+            # print(agg_data)
+
+            reorg_data = agg_data.pivot('Date', 'Ticker', 'Close').reset_index()
+            matrix = reorg_data.corr(method="pearson")
+            matrix.index.name = None
+
+            c_matrix[timeframe] = matrix
+
+        return c_matrix
+
+    def action_signal(self, signal: dict):
+        # print(signal)
+        pass
+
+    def start(self, start_timestamp=None, finish_timestamp=None):
+        """
+        IMPORTANT: all dataframes must be synchronised on date index, i.e have a continuous date index,
+        no missing timestamps, start and finish on same timestamps.
+
+        Limitations:
+            - Simulation supports only a single timeframe across all strategies.
+            - Features requiring analysis of > 1 bar time periods must be condensed into a single bar.
+                see EmaCross50200 for example using Cross column.
+        """
+
+        # Data pre-processing.
         self.apply_features_all_datasets(self.data, self.portfolio.strategies, self.portfolio.assets_flattened)
+        self.c_matrix = self.correlation_matrix(self.data, self.portfolio.timeframes, self.portfolio.assets_flattened)
 
+        # print(self.c_matrix["1d"])
+        # print(self.c_matrix["1d"]['GOOGL']['AMZN'])
+        # print(self.c_matrix["1d"]['F']['TSLA'])
+
+        # Use first asset's data for start/finish indexing.
+        asset_class = list(self.portfolio.assets.keys())[0]
+        symbol = self.portfolio.assets[asset_class][0]
+        timeframe = self.portfolio.strategies[0].timeframe
+        df = self.data[asset_class][symbol][timeframe]
+        rows = df.shape[0]
+
+        start_index = df.iloc[start_timestamp].name if start_timestamp is not None else 0
+        finish_index = df.iloc[start_timestamp].name if finish_timestamp is not None else rows
+
+        # print(self.portfolio.summary())
+        print(f"Running simulation for {self.portfolio.name}...")
+
+        # Iterate dataframes timestamp by timestamp.
+        for index in range(start_index, finish_index - 1):
+            for asset_class in self.portfolio.assets:
+                for symbol in self.portfolio.assets[asset_class]:
+
+                    for strategy in self.portfolio.strategies:
+                        signal = strategy.check_for_signal(
+                            self.data[asset_class][symbol][strategy.timeframe].iloc[index])
+                        if signal:
+                            signal['asset_class'] = asset_class
+                            signal['symbol'] = symbol
+                            signal['timeframe'] = strategy.timeframe
+                            signal['strategy'] = strategy.name
+                            self.action_signal(signal)
