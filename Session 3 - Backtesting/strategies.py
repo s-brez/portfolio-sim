2cea963c9a8c3f837d3b0409613f1618b4d15458
s-brez <sam@sdbgroup.io>
2022-07-24 16:42:39 +1000

    add 'cross' output column to indicate signals in strategy pre-processing

--- Session 3 - Backtesting/strategies.py
+++ Session 3 - Backtesting/strategies.py
@@ -7,15 +7,45 @@ class EMACross50200:
     timeframe = "1d"
 
     def feature_data(data: pd.DataFrame, slow=200, fast=50) -> [pd.Series]:
-        return [
-            ("50EMA", data['Close'].ewm(span=fast, adjust=False).mean()),
-            ("200EMA", data['Close'].ewm(span=slow, adjust=False).mean())]
+        """
+        Use a third "cross" column if-or-not a cross occurred on that row.
+        """
+        slow_ema = data['Close'].ewm(span=slow, adjust=False).mean().rename('EMA200')
+        fast_ema = data['Close'].ewm(span=fast, adjust=False).mean().rename('EMA50')
+        cross = pd.Series(None, index=data.index).rename("Cross")
+
+        # Populate cross column.
+        # Not the fastest way, use a vectorised method if dataframe sizes get much larger.
+        temp_df = pd.concat([fast_ema, slow_ema], axis=1)
+        for index in range(0, temp_df.shape[0]):
+            if index > 0:
+                prev_slow = temp_df.iloc[index - 1]["EMA200"]
+                prev_fast = temp_df.iloc[index - 1]["EMA50"]
+                curr_slow = temp_df.iloc[index]["EMA200"]
+                curr_fast = temp_df.iloc[index]["EMA50"]
+
+                # Buy side check (fast > slow)
+                if (prev_slow > prev_fast) and (curr_fast > curr_slow):
+                    cross.iloc[index] = "BUY"
+
+                # Sell side check (slow > fast)
+                if (prev_slow < prev_fast) and (curr_fast < curr_slow):
+                    cross.iloc[index] = "SELL"
+
+        return [("50EMA", fast_ema), ("200EMA", slow_ema), ("Cross", cross)]
 
     def check_for_signal(data: pd.Series) -> dict:
         """
-        Given a single row, return a signal if one presents or None.
+        Return a signal if one presents, or None.
         """
-        print("yeet")
+        signal = None
+
+        if data['Cross'] == "BUY" or data['Cross'] == "SELL":
+            signal = {
+                'direction': data['Cross']
+            }
+
+        return signal
 
 
 class EMACross1020:
