e80e304e432493f5cc852a2d1100477ac818c6dd
s-brez <sam@sdbgroup.io>
2022-08-03 22:11:34 +1000

    add full menu for test runner

--- Session 4 - Portfolio & DB/test_runner.py
+++ Session 4 - Portfolio & DB/test_runner.py
@@ -5,16 +5,14 @@ import sys
 
 
 def menu():
-    print("\n1: Portfolio performance")
-    # print("2: Strategy performance")
-    # print("3: Equity curve")
-    print("2:")
-    print("3:")
-    print("4: Risk and other parameters")
+    print("1: Parameter summary")
+    print("2: Portfolio performance")
+    print("3: Per-strategy performance")
+    print("4: Display equity curve")
     print("5: Target assets")
     print("6: Asset class and strategy allocations")
-    print("7: Transaction records")
-    print("8: Trade records")
+    print("7: Transaction record")
+    print("8: Trade record")
     print("9: Exit")
 
 
@@ -31,15 +29,13 @@ while(True):
         print("Invalid input")
 
     if option == 1:
-        print(bt.portfolio.metrics())
-    elif option == 2:
-        # print(bt.portfolio.strategy_metrics())
-        pass
-    elif option == 3:
-        # bt.portfolio.equity_curve()
-        pass
-    elif option == 4:
         print(bt.portfolio.parameter_summary())
+    elif option == 2:
+        print(bt.portfolio.metrics())
+    elif option == 3:
+        print(bt.portfolio.strategy_metrics())
+    elif option == 4:
+        bt.portfolio.equity_curve()
     elif option == 5:
         print(json.dumps(bt.portfolio.assets, indent=2))
     elif option == 6:
