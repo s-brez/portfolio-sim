353484f0b4b42ef00bc73c1bd28d8015908fb586
s-brez <sam@sdbgroup.io>
2022-08-04 15:32:50 +1000

    fix string formatting bug in test runner

--- Session 4 - Portfolio & DB/test_runner.py
+++ Session 4 - Portfolio & DB/test_runner.py
@@ -1,14 +1,15 @@
 from portfolios import TestPortfolio
 from backtest import Backtester
+from copy import deepcopy
 import json
 import sys
 
 
 def menu():
-    print("1: Parameter summary")
+    print("1: Portfolio parameter summary")
     print("2: Portfolio performance")
-    print("3: Per-strategy performance")
-    print("4: Display equity curve")
+    print("3: Per-strategy, per-asset, per-timeframe performance")
+    print("4: Portfolio equity curve")
     print("5: Target assets")
     print("6: Asset class and strategy allocations")
     print("7: Transaction record")
@@ -30,20 +31,35 @@ while(True):
 
     if option == 1:
         print(bt.portfolio.parameter_summary())
+
     elif option == 2:
         print(bt.portfolio.metrics())
+
     elif option == 3:
         print(bt.portfolio.strategy_metrics())
+
     elif option == 4:
         bt.portfolio.equity_curve()
+
     elif option == 5:
         print(json.dumps(bt.portfolio.assets, indent=2))
+
     elif option == 6:
         print(json.dumps(bt.portfolio.allocations, indent=2))
+
     elif option == 7:
         print(json.dumps(bt.portfolio.transaction_history, indent=2))
+
     elif option == 8:
-        print(json.dumps(bt.portfolio.trade_history, indent=2))
+
+        # json.dumps wont accept timedelta objects, reformat them to strings.
+        str_trade_history = []
+        for index, trade in enumerate(bt.portfolio.trade_history):
+            str_trade_history.append(deepcopy(trade))
+            str_trade_history[index]['hold_time'] = str(str_trade_history[index]['hold_time'])
+
+        print(json.dumps(str_trade_history, indent=2))
+
     elif option == 9:
         sys.exit(0)
     else:
